7e0d58dd464013c75802e6170d1e673667147280
Robert Farrar <rfarra00@uafs.edu>
2024-01-24 12:49:29 -0600

    Added project

---
+++ first/first/prog.java
@@ -0,0 +1,12 @@
+package first;
+
+public class prog {
+	public static void main(String args[])
+		{
+			int array[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+			for(int i = 0; i < array.length; i++)
+			{
+				System.out.println(array);
+			}
+		}
+}
